321612619416506d61a6fc7b5c2f866d17500aab
Raneem <ranim.krenbeh99@gmail.com>
2026-05-20 00:14:56 +0200

    fix backend urls

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -31,7 +31,7 @@ function Notes() {
       const token = localStorage.getItem("token");
 
       const response = await axios.get(
-        "https://notes-app-backend.onrender.com/api/notes",
+        "https://notes-app-backend-6vkp.onrender.com/api/notes",
         {
           headers: {
             authorization: token,
@@ -50,7 +50,7 @@ function Notes() {
       const token = localStorage.getItem("token");
 
       await axios.post(
-        "https://notes-app-backend.onrender.com/api/notes",
+        "https://notes-app-backend-6vkp.onrender.com/api/notes",
         {
           title,
           content,
@@ -80,7 +80,7 @@ function Notes() {
       const token = localStorage.getItem("token");
 
       await axios.delete(
-        `https://notes-app-backend.onrender.com/api/notes/${id}`,
+        `https://notes-app-backend-6vkp.onrender.com/api/notes/${id}`,
         {
           headers: {
             authorization: token,
@@ -110,7 +110,7 @@ function Notes() {
       const token = localStorage.getItem("token");
 
       await axios.put(
-        `https://notes-app-backend.onrender.com/api/notes/${editingId}`,
+        `https://notes-app-backend-6vkp.onrender.com/api/notes/${editingId}`,
         {
           title,
           content,
